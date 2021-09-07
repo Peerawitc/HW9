a21e0b4eb3ac159f6b0a1c5617c0d4d38272be29
peerawit chinawong <domokungpee10@hotmail.com>
2021-09-07 21:56:36 +0700

    update

--- lib/fontthai.dart
+++ lib/fontthai.dart
@@ -2,13 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 class fontthai extends StatefulWidget {
   const fontthai({Key? key}) : super(key: key);
-
   @override
   _fontthaiState createState() => _fontthaiState();
 }
-
 class _fontthaiState extends State<fontthai> {
-
 List <String> font = ["Sarabun","Taviraj" ,"Mali","Krub","Niramit" ,"Charm", "KoHo" ,"K2D","Chonburi" , "Srisakdi" ];
 var setfont ="Srisakdi";
 Widget _fontbutton(String str){
@@ -24,12 +21,7 @@ Widget _fontbutton(String str){
                 horizontal: 40.0, vertical: 10.0)),
         child: Text(str.toString(),style: TextStyle(color: Colors.black ),)),
  );
-
-
 }
-
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -45,8 +37,6 @@ Widget _fontbutton(String str){
                 .fill, // ยืดเต็มพื้นที่โดยไม่สนใจสัดส่วนของภาพ หรือใช้ BoxFit.cover เพื่อยืดให้เต็มและคงสัดส่วนของภาพไว้ แต่บางส่วนของภาพอาจถูก crop
           ),
         ),
-
-
         child: Center(
 
           child: Column(
@@ -70,14 +60,12 @@ Widget _fontbutton(String str){
                   ),
                 ),
               ),
+              Text("Font: $setfont"),
               Card(
                    color:Color.fromRGBO(38, 38, 38, 0.0),
                 child: Wrap(
                     alignment: WrapAlignment.center,
                   children: font.map((e) => _fontbutton(e)).toList()
-
-
-
                 ),
               )
             ],
